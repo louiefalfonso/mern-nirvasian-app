510ae82a94e992be97c7d4ae35aea8a6981de807
Louie Alfonso <lfriasalfonso@gmail.com>
2024-08-01 15:38:14 +0100

    Added Login Feature with JWT Auth

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 const connectDB = async () => {
     try {
@@ -9,5 +10,18 @@ const connectDB = async () => {
         process.exit(1);
     }
 }
+
+export const createJWT = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+  });
+};
 
 export default connectDB
--- server/index.js
+++ server/index.js
@@ -2,19 +2,49 @@ import express from "express";
 import dotenv from "dotenv";
 import schema from "./schema/schema.js";
 import { graphqlHTTP } from "express-graphql";
-import connectDB from "./config/db.js";
 import cors from "cors";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import { errorHandler, routeNotFound } from "./middlewares/errorMiddlewares.js";
+import routes from "./routes/index.js";
 
+//config
 dotenv.config();
+
+//set port
 const port = process.env.PORT || 5000;
+
+//connect to db
 const app = express();
+
+//connect to db
 connectDB();
 
-const server = app.listen(port, () =>
-  console.log(`Server listening on ${port}`)
-);
-
+//cors
 app.use(cors());
+
+//morgan
+app.use(morgan("dev"));
+
+// Parse incoming requests with JSON payloads
+app.use(bodyParser.json());
+app.use(express.json());
+
+//cookie parser
+app.use(cookieParser());
+
+// Parse incoming requests with urlencoded payloads
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
 app.use(
   "/graphql",
@@ -22,6 +52,18 @@ app.use(
     schema: schema,
     graphiql: process.env.NODE_ENV === "development",
   })
+);
+
+//routes
+app.use("/api", routes);
+
+//error middlewares
+app.use(routeNotFound);
+app.use(errorHandler);
+
+//log requests
+const server = app.listen(port, () =>
+  console.log(`Server listening on ${port}`)
 );
 
 
